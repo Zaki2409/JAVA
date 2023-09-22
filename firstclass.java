c1007b14d257ee642a1487eeffcedf7995bf6f91
zaki khan <mohammedkhanzaki@gmail.com>
2023-09-22 19:44:20 +0530

    DATA TYPES , I/O , VARIABLES

--- firstclass.java
+++ firstclass.java
@@ -1,5 +1,9 @@
 class firstclass {
     public static void main(String args[]) {
+    // BASIC
+    
     System.out.println("hello world");
+    
+
     }
 }
---
+++ ioclass.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class ioclass {
+  public static void main(String argd[] ){
+    Scanner sc = new Scanner(System.in);
+    int a=sc.nextInt();
+    int b=sc.nextInt();
+    int sum = a +b;
+    System.out.print(sum);
+  }  
+}
